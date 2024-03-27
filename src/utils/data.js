7cf8a20c811a52ca701a5bc2df4b23d580aad194
Gigi <gigi151521@gmail.com>
2024-03-27 11:40:00 -0600

    adding data and icons

---
+++ src/utils/data.js
@@ -0,0 +1,53 @@
+export const SKILLS = [
+    {
+        title: 'FrontEnd',
+        icon: './assets/images/frontEnd-icon.png',
+        skills: [
+            { skill: "JavaScript", percentage: '85%' },
+            { skill: "React.js", percentage: '80%' },
+            { skill: "HTML5", percentage: '80%' },
+            { skill: "CSS3 (Sass/SCSS)", percentage: '75%' },
+
+        ]
+    },
+    {
+        title: 'BackEnd',
+        icon: './assets/images/backEnd-icon.png',
+        skills: [
+            { skill: "Node.js", percentage: '75%' },
+            { skill: "Express.js", percentage: '70%' },
+            { skill: "Mongoose", percentage: '70%' },
+
+        ]
+    },
+    {
+        title: 'Testing',
+        icon: './assets/images/test.png',
+        skills: [
+            { skill: "Jest", percentage: '65%' },
+            { skill: "Cypress", percentage: '60%' }
+
+        ]
+    },
+    {
+        title: 'Data Base',
+        icon: './assets/images/database.png',
+        skills: [
+            { skill: "MongoDb", percentage: '70%' },
+            { skill: "MySQL", percentage: '60%' }
+
+        ]
+    },
+    {
+        title: 'Soft Skills',
+        icon: './assets/images/communication.png',
+        skills: [
+            { skill: "Problem-solving", percentage: '85%' },
+            { skill: "Team-player ", percentage: '85%' },
+            { skill: "Attention to Detail", percentage: '80%' },
+
+        ]
+    },
+
+
+]
